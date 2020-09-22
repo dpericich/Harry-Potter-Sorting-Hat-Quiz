b23d8d7890fc1ada0dd5048977d14f65a523a9ba
Daniel <dpericich@gmail.com>
2020-09-21 22:23:26 -0500

    Created JSON structured file to store quiz Q&A

---
+++ src/QuestionComponents/Questions.js
@@ -0,0 +1,33 @@
+// export const quizAnswers = [
+//     [{answer: "Snake", value:1}, {answer: "Badger", value:2}, {answer: "Lion", value:3}, {answer: "Raven", value:4}],
+//     [{answer: "Grass", value:2}, {answer: "Earwax", value:3}, {answer: "Popcorn", value:4}, {answer: "Vomit", value:0}],
+//     [{answer: "Red", value:3}, {answer: "Blue", value:4}, {answer: "Green", value:0}, {answer: "Yellow", value:2}],
+//     [{answer: "Wizard History", value:4}, {answer: "Potions", value:1}, {answer: "Botany", value:2}, {answer: "Defense Against the Dark Arts", value:3}],
+//     [{answer: "Harry Potter", value:1}, {answer: "Ron Weasley", value:2}, {answer: "Draco Malfoy", value:3}, {answer: "Jenny Weasley", value:4}],
+//     [{answer: "Watching paint dry.", value:2}, {answer: "Reminiscing about your bravery", value:3}, {answer: "Reading a good book", value:4}, {answer: "Beating up first years", value:0}],
+//     [{answer: "Dragon Heartstring", value:3}, {answer: "Phoenix feather", value:4}, {answer: "Unicorn tale", value:1}, {answer: "Cores, what are those?", value:2}],
+//     [{answer: "Smart", value:4}, {answer: "Why does it matter what they think?", value:1}, {answer: "Around", value:2}, {answer: "Brave", value:3}]
+// ]
+
+// export const quizQuestions = [
+//     "What is your favorite animal?", 
+//     "What is your favoite flavor of jellybean?", 
+//     "What is your favorite color?",
+//     "What is your favorite class", 
+//     "Who do you least like?",
+//     "What is your favorite activity?",
+//     "What kind of core does your wand have?",
+//     "How would your friends describe you?"
+// ]
+
+export const quizQA = [
+    {question: "What is your favorite animal?", answers: [{answer: "Snake", value:1}, {answer: "Badger", value:2}, {answer: "Lion", value:3}, {answer: "Raven", value:4}]}, 
+    {question: "What is your favoite flavor of jellybean?", answers: [{answer: "Grass", value:2}, {answer: "Earwax", value:3}, {answer: "Popcorn", value:4}, {answer: "Vomit", value:0}]}, 
+    {question: "What is your favorite color?", answers : [{answer: "Red", value:3}, {answer: "Blue", value:4}, {answer: "Green", value:0}, {answer: "Yellow", value:2}]},
+    {question: "What is your favorite class", answers :  [{answer: "Wizard History", value:4}, {answer: "Potions", value:1}, {answer: "Botany", value:2}, {answer: "Defense Against the Dark Arts", value:3}]},
+    {question: "Who do you least like?", answers: [{answer: "Harry Potter", value:1}, {answer: "Ron Weasley", value:2}, {answer: "Draco Malfoy", value:3}, {answer: "Jenny Weasley", value:4}]},
+    {question: "What is your favorite activity?", answers: [{answer: "Watching paint dry.", value:2}, {answer: "Reminiscing about your bravery", value:3}, {answer: "Reading a good book", value:4}, {answer: "Beating up first years", value:0}]},
+    {question: "What kind of core does your wand have?", answers: [{answer: "Dragon Heartstring", value:3}, {answer: "Phoenix feather", value:4}, {answer: "Unicorn tale", value:1}, {answer: "Cores, what are those?", value:2}]},
+    {question: "How would your friends describe you?", answers: [{answer: "Smart", value:4}, {answer: "Why does it matter what they think?", value:1}, {answer: "Around", value:2}, {answer: "Brave", value:3}]}
+
+]
