54e632e06b15818b63eacb101812c1e3da8abecc
Daniel <dpericich@gmail.com>
2020-09-22 21:29:28 -0500

    added id to answers to help identify them for scores

--- src/QuestionComponents/Questions.js
+++ src/QuestionComponents/Questions.js
@@ -21,13 +21,13 @@
 // ]
 
 export const quizQA = [
-    {question: "What is your favorite animal?", answers: [{answer: "Snake", value:1}, {answer: "Badger", value:2}, {answer: "Lion", value:3}, {answer: "Raven", value:4}]}, 
-    {question: "What is your favoite flavor of jellybean?", answers: [{answer: "Grass", value:2}, {answer: "Earwax", value:3}, {answer: "Popcorn", value:4}, {answer: "Vomit", value:0}]}, 
-    {question: "What is your favorite color?", answers : [{answer: "Red", value:3}, {answer: "Blue", value:4}, {answer: "Green", value:0}, {answer: "Yellow", value:2}]},
-    {question: "What is your favorite class", answers :  [{answer: "Wizard History", value:4}, {answer: "Potions", value:1}, {answer: "Botany", value:2}, {answer: "Defense Against the Dark Arts", value:3}]},
-    {question: "Who do you least like?", answers: [{answer: "Harry Potter", value:1}, {answer: "Ron Weasley", value:2}, {answer: "Draco Malfoy", value:3}, {answer: "Jenny Weasley", value:4}]},
-    {question: "What is your favorite activity?", answers: [{answer: "Watching paint dry.", value:2}, {answer: "Reminiscing about your bravery", value:3}, {answer: "Reading a good book", value:4}, {answer: "Beating up first years", value:0}]},
-    {question: "What kind of core does your wand have?", answers: [{answer: "Dragon Heartstring", value:3}, {answer: "Phoenix feather", value:4}, {answer: "Unicorn tale", value:1}, {answer: "Cores, what are those?", value:2}]},
-    {question: "How would your friends describe you?", answers: [{answer: "Smart", value:4}, {answer: "Why does it matter what they think?", value:1}, {answer: "Around", value:2}, {answer: "Brave", value:3}]}
+    {question: "What is your favorite animal?", answers: [{answer: "Snake", value:1, id: 1}, {answer: "Badger", value:2, id: 2}, {answer: "Lion", value:3, id:3}, {answer: "Raven", value:4, id: 4}]}, 
+    {question: "What is your favoite flavor of jellybean?", answers: [{answer: "Grass", value:2, id: 5}, {answer: "Earwax", value:3, id:6}, {answer: "Popcorn", value:4, id:7}, {answer: "Vomit", value:0, id:8}]}, 
+    {question: "What is your favorite color?", answers : [{answer: "Red", value:3, id:9}, {answer: "Blue", value:4, id:10}, {answer: "Green", value:0, id:11}, {answer: "Yellow", value:2, id: 12}]},
+    {question: "What is your favorite class", answers :  [{answer: "Wizard History", value:4, id:13}, {answer: "Potions", value:1, id:14}, {answer: "Botany", value:2, id:15}, {answer: "Defense Against the Dark Arts", value:3, id:16}]},
+    {question: "Who do you least like?", answers: [{answer: "Harry Potter", value:1, id:17}, {answer: "Ron Weasley", value:2, id:18}, {answer: "Draco Malfoy", value:3, id:19}, {answer: "Jenny Weasley", value:4, id:20}]},
+    {question: "What is your favorite activity?", answers: [{answer: "Watching paint dry.", value:2, id:21}, {answer: "Reminiscing about your bravery", value:3, id:22}, {answer: "Reading a good book", value:4, id:23}, {answer: "Beating up first years", value:0, id:24}]},
+    {question: "What kind of core does your wand have?", answers: [{answer: "Dragon Heartstring", value:3, id:25}, {answer: "Phoenix feather", value:4, id:26}, {answer: "Unicorn tale", value:1, id:27}, {answer: "Cores, what are those?", value:2, id:28}]},
+    {question: "How would your friends describe you?", answers: [{answer: "Smart", value:4, id:29}, {answer: "Why does it matter what they think?", value:1, id:30}, {answer: "Around", value:2, id:31}, {answer: "Brave", value:3, id:32}]}
 
 ]
